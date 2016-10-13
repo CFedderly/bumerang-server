2c58e34c6a4106543662e55b2ac1ce1ba9a85e7c
Tyler Potter <tjpotter@uvic.ca>
2016-10-12 23:48:43 -0700

    Style changes for pep8
    Added some style changes so the code conforms to pep8

--- src/bumerang/profile.py
+++ src/bumerang/profile.py
@@ -1,25 +1,26 @@
-from tornado.web import RequestHandler
 from tornado.escape import json_decode
-from urllib.parse import urlparse, parse_qs
-from json import dumps, loads		
+from tornado.web import RequestHandler
 
 default_profile = {
-	"firstname": "John",
-	"lastname": "Doe",
-	"description": "I like pina coladas and getting caught in the rain",
-	"tags": "pina coladas, rain"
+    'firstname': 'John',
+    'lastname': 'Doe',
+    'description': 'I like pina coladas and getting caught in the rain',
+    'tags': 'pina coladas, rain'
 }
 
-class ProfileHandler(RequestHandler):
-	
-	""" Included a get for a profile with an id in case we want to
-		eventually look at another user's profile."""
-	def get(self, id=None):
-		if id is not None:
-			self.write(id + " Get profile")
-		else:
-			self.write(default_profile)
 
-	def post(self):
-		parsed_json = json_decode(self.request.body)
-		self.write(parsed_json)
+class ProfileHandler(RequestHandler):
+
+    def get(self, id=None):
+        """ Included a get for a profile with an id in case we want to
+
+            eventually look at another user's profile.
+        """
+        if id is not None:
+            self.write(id + " Get profile")
+        else:
+            self.write(default_profile)
+
+    def post(self):
+        parsed_json = json_decode(self.request.body)
+        self.write(parsed_json)
