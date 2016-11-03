d7f73902578f7b2d0c4ff9584e75b6dff59655ba
Tyler Potter <tjpotter@uvic.ca>
2016-11-03 00:31:05 -0700

    Add phone_number and karma to profiles
    Profiles needed the phone number for users to contact eachother and karma for
    validation of users. requests was added to requirements.txt for the
    notifications.

--- src/bumerang/profile/profile.py
+++ src/bumerang/profile/profile.py
@@ -3,7 +3,7 @@ class Profile:
     def __init__(
             self, id, facebook_id,
             device_id, first_name, last_name,
-            description):
+            description, phone_number, karma):
         """Create our profile object"""
         self.id = id
         self.facebook_id = facebook_id
@@ -11,6 +11,8 @@ class Profile:
         self.first_name = first_name
         self.last_name = last_name
         self.description = description
+        self.phone_number = phone_number
+        self.karma = karma
 
     def __eq__(self, other):
         return self.id == other.id
@@ -18,7 +20,8 @@ class Profile:
     def __repr__(self):
         return 'Profile(%r, %r, %r, %r, %r, %r)' % (
             self.id, self.facebook_id, self.device_id,
-            self.first_name, self.last_name, self.description
+            self.first_name, self.last_name, self.description,
+            self.phone_number, self.karma
         )
 
     def to_node(self):
@@ -30,6 +33,8 @@ class Profile:
                 'device_id': self.device_id,
                 'first_name': self.first_name,
                 'last_name': self.last_name,
-                'description': self.description
+                'description': self.description,
+                'phone_number': self.phone_number,
+                'karma': self.karma
             }
         }
